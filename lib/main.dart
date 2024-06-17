5867da2980143b1b723797f2bca766c839a85c9e
Sandydht <sandydwi75@gmail.com>
2024-06-17 11:07:12 +0700

    feat: switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Column',
-            style: TextStyle(color: Colors.white),
+      title: 'Switch',
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  bool switchStatus = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text(
+          'Switch',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold
           ),
-          backgroundColor: Colors.blueAccent,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
           children: [
-            Container(
-              // width: 200,
-              height: 50,
-              color: Colors.green,
+            Switch.adaptive(
+              value: switchStatus,
+              onChanged: (value) {
+                setState(() {
+                  switchStatus = !switchStatus;
+                });
+              }
             ),
-            Container(
-              // width: 50,
-              height: 50,
-              color: Colors.blue,
-            ),
-            Container(
-              // width: 100,
-              height: 50,
-              color: Colors.amber,
-            ),
-            Container(
-              // width: 300,
-              height: 50,
-              color: Colors.red,
+            Text(
+              switchStatus ? 'Switch On' : 'Switch Off'
             )
-          ]
-        ),
+          ],
+        )
       ),
     );
   }
